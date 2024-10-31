a97dbe192516f27fe1430f388837e70d6c5db459
Sjoerd4435 <prowebstore126@gmail.com>
2024-11-01 00:14:22 +0100

    Update download.js

--- download.js
+++ download.js
@@ -1,6 +1,6 @@
 function downloadZip() {
     const link = document.createElement('a');
-    link.href = 'https://github.com/Sjoerd4435/Finnify/archive/refs/heads/main.zip'; // Link to the main.zip file
+    link.href = 'https://github.com/Sjoerd4435/Finnify/archive/refs/heads/main/Finnify'; // Link to the main.zip file
     link.download = 'Finnify.zip'; // Desired download name
     document.body.appendChild(link);
     link.click();
